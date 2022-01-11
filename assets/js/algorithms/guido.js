8ad0529bc4e1e9d240dc35a077e634c590e14e40
Stephen Gardener <stephen@noddlepod.com>
2022-01-11 13:56:29 +0100

    Pass in default values to pitch_select

--- assets/js/algorithms/guido.js
+++ assets/js/algorithms/guido.js
@@ -2,6 +2,12 @@ let myPhrase, myPart;
 // let pattern = ['F3','E3','G3','C3'];
 
 function setup() {
+  // set up tooltips
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
   const notesInput   = document.querySelector('#notesInput');
   const notesDisplay = document.querySelector('#notesDisplay');
   const genNotesBtn  = document.querySelector('#genNotes')
@@ -22,22 +28,80 @@ function playGeneratedNotes() {
 }
 
 function generateNotes(e) {
-  let fullText = notesInput.value
+  let fullText = notesInput.value;
 
-  var vf = new Vex.Flow.Factory({renderer: {elementId: 'score'}});
-  var score = vf.EasyScore();
-  var system = vf.System();
+  pattern = map_pitches(fullText);
 
-  system.addStave({
-    voices: [score.voice(score.notes('C#5/q, B4, A4, G#4'))]
-  }).addClef('treble').addTimeSignature('4/4');
+  // map_durations(fullText);
 
-  vf.draw();
+  console.log(pattern);
+
+  const data = `
+    tabstave notation=true
+
+    notes :q =|: (5/2.5/3.7/4) :8 7-5h6/3 ^3^ 5h6-7/5 ^3^ :q 7V/4 |
+    notes :8 t12p7/4 s5s3/4 :8 3s:16:5-7/5 :q p5/4
+    text :w, |#segno, ,|, :hd, , #tr
+  `
+
+  const VF = vextab.Vex.Flow
+  const renderer = new VF.Renderer($('#boo')[0],
+    VF.Renderer.Backends.SVG);
+
+  // Initialize VexTab artist and parser.
+  const artist = new vextab.Artist(10, 10, 750, { scale: 0.8 });
+  const tab = new vextab.VexTab(artist);
+
+  tab.parse(data);
+  artist.render(renderer);
+
+  // var vf = new Vex.Flow.Factory({renderer: {elementId: 'score'}});
+  // var score = vf.EasyScore();
+  // var system = vf.System();
+
+  // system.addStave({
+  //   voices: [score.voice(score.notes(pattern.join(', '))).setStrict(false)]
+  // }).addClef('treble').addTimeSignature('4/4');
+
+  // vf.draw();
 
   // monoSynth = new p5.MonoSynth();
   // myPhrase  = new p5.Phrase('bbox', onEachStep, pattern);
   // myPart    = new p5.Part();
   // myPart.addPhrase(myPhrase);
   // myPart.setBPM(60);
-  notesDisplay.textContent = fullText;
+  // notesDisplay.textContent = fullText;
+}
+
+function build_note_table() {
+  return note_table = {
+    'a': document.querySelector('#pitch-select-a').value,
+    'e': document.querySelector('#pitch-select-e').value,
+    'i': document.querySelector('#pitch-select-i').value,
+    'o': document.querySelector('#pitch-select-o').value,
+    'u': document.querySelector('#pitch-select-u').value
+  };
+}
+
+function map_pitches(text) {
+  note_table = build_note_table();
+  pattern    = [];
+  words = text.toLowerCase().split(' ');
+  words.forEach(function(word, index) {
+    for (let char of word) {
+      if (is_vowel(char)) {
+        pitch = note_table[char]
+        pattern.push(pitch);
+      }
+    }
+  });
+  return pattern.map(note => { return note + '/4'});
+}
+
+function map_durations(text) {
+
+}
+
+function is_vowel(char) {
+  return (char.match(/[aeiou]/)) ? true : false
 }
