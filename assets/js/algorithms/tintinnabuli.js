f20dbdee154e873697fea30e72d263cabdbc1382
Stephen Gardener <stephen@noddlepod.com>
2022-03-28 19:26:15 +0200

    tintinn updates

--- assets/js/algorithms/tintinnabuli.js
+++ assets/js/algorithms/tintinnabuli.js
@@ -3,12 +3,119 @@ var score  = new Score("Tintinnabuli")
 window.onload = (event) => {
   document.querySelector('#genNotes').onclick  = generateNotes
   document.querySelector('#play').onclick = playGeneratedNotes
-  // document.querySelector('#order').oninput = updateOrder
-  // document.querySelector('#bpm').oninput = updateBPM
-  // document.querySelector('button#tune1').onclick = play
-  // document.querySelector('button#tune2').onclick = play
-  // document.querySelector('button#tune3').onclick = play
-  // document.querySelector('button#tune4').onclick = play
-  // document.querySelector('button#tune5').onclick = play
-  // document.querySelector('button#tune6').onclick = play
+  document.querySelector('#bpm').oninput = updateBPM
+
+  document.querySelector('#seq-plus').onclick = seqPlus
+  document.querySelector('#seq-minus').onclick = seqMinus
+}
+
+function seqPlus(e){
+  document.querySelector('#seq-btns')
+}
+function seqPlus(e){
+}
+
+function generateNotes(e) {
+  score.clear()
+
+  // get selected key
+  var key = document.querySelector('#key-select').value
+  var scaleType = document.querySelector('input[name="scale-type-radio"]:checked').value
+  score.key = key + scaleType
+  score.noteLength = document.querySelector('#rhythm-select option:checked').text
+  score.tempo = document.querySelector('#bpm').value
+
+  var direction = document.querySelector('#direction-select').value
+  var octave = document.querySelector('#octave-select').value
+  console.log(octave)
+
+  var scale   = genScale(key)
+  scale       = scale.concat(scale).concat(scale);
+  var noteLen = noteLengthPattern([1,0,1,1,0])
+  var mPart   = scalePattern(scale, [1], direction)
+  var tPart   = genChordTones(key, octave)
+
+  for(const [index,note] of mPart.entries()){
+    // get note length from noteLen pattern
+    var lenIndex = (index) % noteLen.length;
+    var len = noteLen[lenIndex];
+    score.addNote(note, len.toString(), 0)
+    // pick random note from T Part
+    tIndex = Math.floor(Math.random() * (2 + 1))
+    score.addNote(tPart[tIndex], len.toString(), 1)
+    lenIndex++
+  }
+  // console.log(score)
+
+  score.render()
+}
+
+function noteLengthPattern(pattern) {
+  var lens = []
+  // calc note lengths
+  for(const [index,trig] of pattern.entries()){
+    if(trig == 1){
+      var len = 1
+      for(var j = index+1; j < pattern.length; j++){
+        if(pattern[j] == 0){
+          len++
+        } else {
+          break
+        }
+      }
+      lens.push(len)
+    }
+  }
+  console.log(lens)
+  return lens
+}
+
+function genChordTones(key, octave) {
+  var chordTones = []
+  var notes = ['A','B','C','D','E','F','G']
+  var notesIndex = notes.indexOf(key)
+  for(var i=0; i < 3; i++){
+    chordTones[i] = notes[notesIndex] + octave
+    notesIndex = (notesIndex + 2) % notes.length
+  }
+  console.log('chordTones')
+  console.log(chordTones)
+  return chordTones
+}
+
+function genScale(key){
+  var scale = []
+  var notes = ['A','B','C','D','E','F','G']
+  var notesIndex = notes.indexOf(key)
+  var octave = ''
+  for(var i=0; i < 8; i++){
+    if(notes[notesIndex] == 'C' && i!==0){
+      octave = '\''
+    }
+    scale[i] = notes[notesIndex] + octave
+    notesIndex = (notesIndex + 1) % notes.length
+  }
+  return scale
+}
+
+function scalePattern(scale, pattern, direction){
+  if(direction == 'down'){
+    scale = scale.reverse()
+  }
+
+  newScale = []
+  var patIndex = 0
+  for(const [index,val] of scale.entries()){
+    pattern[patIndex] == 1 && newScale.push(val)
+    var patIndex = (index + 1) % pattern.length
+  }
+  return newScale
+}
+
+function playGeneratedNotes(e) {
+  AudioPlayer.play(score.toABC())
+}
+
+function updateBPM(e) {
+  document.querySelector('#bpm-display').innerHTML = e.target.value
 }
--- assets/js/score.js
+++ assets/js/score.js
@@ -22,105 +22,14 @@ class Score {
     this.score = [[],[]];
   }
 
-  // -------------------
-  // IMPORT ABC METHODS
-  // -------------------
-  importABC(abc) {
-    this.title = this.getTitle(abc);
-    this.meter = this.getMeter(abc);
-    this.tempo = this.getTempo(abc);
-    this.key = this.getKey(abc);
-    this.importNotes(abc);
-  }
-  getTitle(abc){
-    var reggy = /^T:.+$/gm
-    return reggy.exec(abc)[0].split(":")[1];
-  }
-  getMeter(abc){
-    var reggy = /^M:\d\/\d$/gm
-    return reggy.exec(abc)[0].split(":")[1];
-  }
-  getTempo(abc){
-    var reggy = /^Q:\d{2,3}$/gm
-    return reggy.exec(abc)[0].split(":")[1];
-  }
-  getKey(abc){
-    var reggy = /^K:.+$/gm
-    return reggy.exec(abc)[0].split(":")[1];
-  }
-  importNotes(abc){
-    // extract notes from ABC file
-    var reggy        = /^(?![A-Za-z]:)[A-Ga-g|\(]/gm
-    var notes_string = abc.substring(abc.search(reggy))
-
-    // The following logic extracts pitch and duration for each note.
-    // See https://abcwiki.org/abc:syntax for ABC syntax.
-    for (var i = 0; i < notes_string.length; ++i){
-      var note = ""
-      var len  = ""
-
-      if(this.isAccidental(notes_string[i])){
-        note += notes_string[i]
-        i++
-      }
-      if(this.isNote(notes_string[i])){
-        note += notes_string[i]
-        if(this.isOctave(notes_string[i+1])){
-          i++
-          note += notes_string[i]
-        }
-
-        if(this.isDuration(notes_string[i+1])){
-          len += notes_string[i+1]
-          if(this.isDuration(notes_string[i+2])){
-            len += notes_string[i+2]
-            i++
-            if(this.isDuration(notes_string[i+2])){
-              len += notes_string[i+2]
-              i++
-            }
-          }
-        } else {
-          // No duration given, default to 1
-          var len = '1'
-        }
-        this.addNote(note, len)
-      }
-    }
-  }
-  isNote(str){
-    var note_reggy = /[A-Ga-gz]/
-    return note_reggy.test(str)
-  }
-  isOctave(str){
-    var note_reggy = /[,']/
-    return note_reggy.test(str)
-  }
-  isAccidental(str){
-    var note_reggy = /^[_=^]/
-    return note_reggy.test(str)
-  }
-  isDuration(str){
-    var num_reggy = /[0-9\/]/
-    return num_reggy.test(str)
-  }
-  // -----------------------
-  // END IMPORT ABC METHODS
-  // -----------------------
-
-  // export score as text for Markov
-  // TODO: if we use SPN (or some such), this is where we'd need to convert it back to HH for display
+  // export score as text - used for Markov
+  // takes the notes of the first voice and creates a string
+  // could make more flexible by adding voice param, but not currently needed
   scoreAsText() {
     var txt = ""
-    for(const [i,voice] of this.score.entries()) {
-      // check if the current voice has any notes
-      if(voice.length > 0) {
-        // add the voices notes to output
-        // txt += "V:"
-        for(const [index,event] of voice.entries()) {
-          txt += event.pitch + event.duration + ' '
-        }
-      }
+    // add the first voices notes to output
+    for(const [index,event] of this.score[0].entries()) {
+      txt += event.pitch + event.duration + ' '
     }
     return txt
   }
@@ -135,51 +44,36 @@ class Score {
     abcString +="T:" + this.title + "\n"
     abcString +="M:" + this.meter + "\n"
     abcString +="Q:" + this.tempo + "\n"
+    abcString +="L:" + this.noteLength + "\n"
     abcString +="K:" + this.key + "\n"
 
-    var bar_count = 0.0
-    var bar = 0
     for(const [i,voice] of this.score.entries()) {
       if(voice.length > 0) {
-        abcString += "V:"
+        abcString += "[V:" + i + " clef=" + this.getClef(voice) + "] "
         for(const [index,event] of voice.entries()) {
-          // keep track of note lengths in order to add bars to score
-          // TODO: maybe we dont need to do this? Bar lines are making things look messy
-          if(this.bars){
-            // get the ABCs note length syntax into a format we can use (a float)
-            var note_len = event.duration
-            if(note_len == '/2'){
-              note_len = '0.5'
-            } else if(note_len == '3/2'){
-              note_len = '1.5'
-            }
-            bar_count += parseFloat(note_len)
-            if(bar_count > 8){
-              bar_count = bar_count - 8
-              abcString += '|'
-              bar++
-              // add line break every 4 bars
-              if(bar == 4) {
-                abcString += '\n'
-                bar = 0
-              }
-            }
-          } else {
-            // no barlines, so limit to 20 notes per line to keep things readable
-            if(index > 0 && index % 20 == 0) {
-              abcString += '|'
-              abcString += '\n'
-            }
+          // limit to 20 notes per line to keep things readable
+          if(index > 0 && index % 20 == 0) {
+            abcString += '|'
+            abcString += '\n'
           }
           abcString += event.pitch + event.duration
         }
         // add double bar line to end
-        // console.log(abcString)
         abcString += '||'
         abcString += '\n'
       }
     }
+    // console.log(abcString)
     return abcString
+  }
+
+  getClef(notes) {
+    // use bass clef if more than 50% notes are down an octave
+    var bassNotes = 0
+    for(var note of notes) {
+      (note.pitch.endsWith(',')) && bassNotes++
+    }
+    return (bassNotes > (notes.length/2)) ? 'bass' : 'treble'
   }
 
   hasNotes() {
